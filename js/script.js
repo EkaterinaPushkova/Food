5c8b2488ebcc3ff1971a9b46e8d2e900dffbdbc7
Ekaterina <pushkova1666@gmail.com>
2021-09-02 00:49:22 +0300

    add selectors for tabs

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {   //constuction for correct loaded page
+    const tabs = document.querySelectorAll('.tabheader__item'),   //tab
+          tabsContent = document.querySelectorAll('.tabcontent'), //content tab
+          tabParent = document.querySelector('.tabheader__items'); //block of tabs
+});
