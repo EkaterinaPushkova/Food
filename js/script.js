8108264c960f489a102613bf9a9e70a85cbb9bc8
Ekaterina <pushkova1666@gmail.com>
2021-09-02 22:42:29 +0300

    create 2 functions for hide/show tabs

--- js/script.js
+++ js/script.js
@@ -2,4 +2,25 @@ window.addEventListener('DOMContentLoaded', () => {   //constuction for correct 
     const tabs = document.querySelectorAll('.tabheader__item'),   //tab
           tabsContent = document.querySelectorAll('.tabcontent'), //content tab
           tabParent = document.querySelector('.tabheader__items'); //block of tabs
+
+    function hideTabContent() {                            //function for hide tab (hide from display and off active mode)
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+         });
+
+         tabsContent.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+         });
+     }
+     
+     function showTabContent(i) {                           //function for show the [i] tabContent and on actiive mode
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+     }
+
+hideTabContent();
+showTabContent(1);
+
+
 });
+
